26957eda00b2735fe91dc22f814620ca1e727d24
KaspianDev <kacperleaguenine@gmail.com>
2023-11-05 14:45:48 +0100

    initial commit

---
+++ src/main/java/com/github/kaspiandev/minedownpapi/MineDownPapi.java
@@ -0,0 +1,34 @@
+package com.github.kaspiandev.minedownpapi;
+
+
+import de.themoep.minedown.MineDown;
+import me.clip.placeholderapi.PlaceholderAPI;
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import net.md_5.bungee.api.chat.BaseComponent;
+import org.bukkit.OfflinePlayer;
+import org.jetbrains.annotations.NotNull;
+
+public final class MineDownPapi extends PlaceholderExpansion {
+
+    @Override
+    public @NotNull String getIdentifier() {
+        return "minedown";
+    }
+
+    @Override
+    public @NotNull String getAuthor() {
+        return "Kaspian";
+    }
+
+    @Override
+    public @NotNull String getVersion() {
+        return "1";
+    }
+
+    @Override
+    public @NotNull String onRequest(OfflinePlayer player, @NotNull String params) {
+        String parsedString = PlaceholderAPI.setBracketPlaceholders(player, params);
+        return BaseComponent.toLegacyText(new MineDown(parsedString).toComponent());
+    }
+
+}
